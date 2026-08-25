699f35a8d362cbc78453497d8c20fdb2b01f5eb6
TAMILARASAN <25cu0320004@student.hindustanuniv.ac.in>
2026-08-25 18:09:35 +0530

    Remove inherited Sanity SEO build plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression';
-import { generateSeoHtml } from './seo-plugin.js';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), viteCompression(), generateSeoHtml()],
+  plugins: [react(), viteCompression()],
   server: {
     proxy: {
       '/sanity-cdn': {
